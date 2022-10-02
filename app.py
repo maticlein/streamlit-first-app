2dc3a20c02303214c3b02ce6a2beb6929191ce7f
Matthias Clein <m.cleinespinoza@gmail.com>
2022-10-02 01:50:09 -0300

    add img and Google Sheets queries

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from gsheetsdb import connect
+from PIL import Image
 
 conn = connect()
 
@@ -12,8 +13,11 @@ def run_query(query):
 
 def main():
     st.title("First Streamlit web app")
+    image = Image.open("./img/logo-innovacion-ufro.png")
+    st.image(image, width = 320)
     st.write("Ejemplo de web app utilizando el framework Streamlit para mostrar las coordenadas del edificio de la Macrofacultad en el campus Andrés Bello de la Universidad de La Frontera, donde se encuentra la oficina del equipo de Innovación Curricular de la Facultad de Ingeniería y Ciencias.")
     st.markdown("## Coordenadas")   
+    st.write("Ejemplo de uso de DataFrame, columnas de Streamlit, métricas y mapa.")
     macrofacultad = pd.DataFrame({
         "lat": [-38.745943943240675],
         "lon": [-72.61618078412191], 
@@ -25,10 +29,19 @@ def main():
     st.markdown("## Mapa")
     st.map(macrofacultad)
 
+    st.markdown("## Equipo Innovación Curricular FICA")
+    st.write("Ejemplo de conexión con Google Sheets.")
+    st.markdown("### a) Resultado de la Query:")
+    st.code("SELECT * FROM sheet_url", language = "sql")
     sheet_url = st.secrets["public_gsheets_url"]
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
     for row in rows:
-        st.write(f"{row.nombre} {row.apellido} {int(row.id)}")
+        st.write(f"{int(row.id)}.- {row.nombre} {row.apellido}")
+    
+    st.markdown("### b) Resultado de la Query:")
+    st.code("SELECT * FROM sheet_url WHERE id = 1", language = "sql")
+    data = run_query(f'SELECT * FROM "{sheet_url}" WHERE id = 1')
+    data
  
 if __name__ == "__main__":
     main()
